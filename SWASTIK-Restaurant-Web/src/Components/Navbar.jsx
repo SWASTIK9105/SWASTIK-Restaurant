6577aa0769262ec869b1e87d4ce12d54ea08047e
SWASTIK TRIPATHI <164197608+SWASTIK9105@users.noreply.github.com>
2025-09-01 08:58:05 +0530

    Update Navbar.jsx

--- SWASTIK-Restaurant-Web/src/Components/Navbar.jsx
+++ SWASTIK-Restaurant-Web/src/Components/Navbar.jsx
@@ -1,19 +1,30 @@
-import react from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux';
 import { setSearch } from '../Redux/Slices/searchSlice';
 import { FaTelegram } from "react-icons/fa";
+import logo from '../assets/logo.jpeg';
 
 const Navbar = () => {
   const dispatch = useDispatch();
   return (
     <nav className='flex flex-col lg:flex-row justify-between mx-6 py-3 mb-12'>
       <div>
-        <h3 className='text-xl font-bold text-gray-600'>
+        <h3 className='text-xl font-bold text-gray-600 mb-2'>
           {new Date().toUTCString().slice(0, 16)}
         </h3>
-        <h1 className='text-2xl font-bold'>
-          SWASTIK Restaurants
-        </h1>
+
+        {/* Logo + Text in flex row */}
+        <div className="flex items-center gap-3 mb-2">
+          <img 
+            src={logo} 
+            alt="SWASTIK-Restaurant" 
+            className="w-10 h-10 object-cover rounded-full"
+          />
+          <h1 className='text-2xl font-bold'>
+            SWASTIK Restaurants
+          </h1>
+        </div>
+
         <a 
           href="https://t.me/SWASTIK_tripathibot" 
           target="_blank" 
@@ -25,13 +36,13 @@ const Navbar = () => {
         </a>
       </div>
 
-      <div>
+      <div className="mt-4 lg:mt-0">
         <input
           onChange={(e) => dispatch(setSearch(e.target.value))}
           type="search"
           placeholder='Search Here'
           autoComplete='off'
-          className='p-3 border border-grey-400 text-sm rounded-lg outline-none w-full lg:w-[75vh]'
+          className='p-3 border border-gray-400 text-sm rounded-lg outline-none w-full lg:w-[75vh]'
         />
       </div>
     </nav>
